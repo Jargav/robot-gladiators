2d75dc1a20d68234a9d422c56282f7469bfc7e2a
Jared Gavigan <jagavigan80@gmail.com>
2021-08-31 18:50:33 -0400

    built FIGHT

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,6 +13,9 @@ var fight = function() {
   // Alert players that they are starting the round  
   window.alert("Welcome to Robot Gladiators!");
 
+  if (promptFight === "fight" || promptFight === "FIGHT") {
+      // remove enemy's health by subtracting the amount set in the playerAttack variable
+
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
   enemyHealth = enemyHealth - playerAttack;
 
@@ -29,6 +32,7 @@ var fight = function() {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
 
+   // remove player's health by subtracting the amount set in the enemyAttack variable
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack;
   // Log a resulting message to the console so we know that it worked.
@@ -39,10 +43,16 @@ var fight = function() {
   // check player's health
 if (playerHealth <= 0) {
     window.alert(playerName + " has died!");
-  } 
-  else {
+  } else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
+// if player choses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    window.alert(playerName + " has chosen to skip the fight!");
+  } else {
+    window.alert("You need to choose a valid option. Try again!");
+  }
+
 };
 
 fight();
